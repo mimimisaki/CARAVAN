1fc8e9e368a08b927b7f64ccf2910a090712fb45
mimimisaki <mimimisaki2@gmail.com>
2020-04-23 15:06:09 +0900

    [Finish] 4-4

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_020056) do
+ActiveRecord::Schema.define(version: 2020_04_23_053431) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
 end
